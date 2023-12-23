cbe3130fd41ca93834b6627e845ec50c849e6675
sharma03aman <154691555+sharma03aman@users.noreply.github.com>
2023-12-23 21:45:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('HAPPY BIRTHDAY AMAN', divider='rainbow')
+
+st.image('img.jpg')
+
+
+st.balloons()
+st.audio('audio.mp3')
+
+if st.button("Press This"):    
+    st.balloons()
